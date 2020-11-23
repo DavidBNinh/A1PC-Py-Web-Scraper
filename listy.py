85c851400c72ac789e4a3bddb7d01b669d8c1623
alexanderchau1 <60366945+alexanderchau1@users.noreply.github.com>
2020-11-23 01:55:37 -0600

    Add files via upload

---
+++ listy.py
@@ -0,0 +1,7 @@
+baseurl = ['https://dma.org/','https://crowcollection.org/','https://www.kimbellart.org/', 'https://www.granadatheater.com/', 'https://www.eisemanncenter.com']
+url = ['https://dma.org/art/exhibitions', 'https://crowcollection.org/exhibitions/', 'https://www.kimbellart.org/art-architecture/exhibitions', 'https://www.granadatheater.com/calendar/', 'https://www.eisemanncenter.com/Events-Tickets/Streaming-Events']
+loca = ['Dallas Arts Museum', 'Crow Collection', 'Kimbell Arts Museum', 'Granada Theater', 'Eismann Center']
+
+print(len(baseurl))
+print(len(url))
+print(len(loca))
